e689fa38c902498ead76cf097d7a84e0132f596f
Christopher Dennis <cdennis.aus@gmail.com>
2018-06-12 19:21:29 -0500

    Updated github repository link

--- app/Components/Footer/Footer.js
+++ app/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='footer'>
       <a href='https://christopherdennis.me/webflix/'><h3>WEBFLIX</h3></a>
-      <a href='https://github.com/chris-topher-d' target='_blank'>GitHub</a>
+      <a href='https://github.com/chris-topher-d/webflix' target='_blank'>GitHub</a>
     </div>
   );
 }
